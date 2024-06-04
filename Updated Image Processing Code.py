ce4fce20533b17c03d5ef5c9f8dcab37472f2de5
Phillip Dominguez <Padom02@gmail.com>
2024-06-04 03:00:07 -0400

    Added the requisite processing to create histograms and return them from the file_handling function.

--- Updated Image Processing Code.py
+++ Updated Image Processing Code.py
@@ -38,6 +38,10 @@ def file_handling():
     h770_list = []
     h780_list = []
 
+    h760 = 0
+    h770 = 0
+    h780 = 0
+
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
     # Construct the full path to the file
@@ -45,27 +49,48 @@ def file_handling():
 
     count = 0
 
+    # This loop will take each file, turn it into a histogram, and then compare the original file name with the string subset to sort the histogram
+
     for file in os.listdir(folder_path):
+        # Convert the file into a histogram by passing the file path to the function
+        img_file = poop.imread(os.path.join(folder_path, file), 0)
+        h_img = poop.calcHist([img_file], [0], None, [256], [0, 256])
+        
+        # Sort the files by nanometers, this is specifed in the file name
         filename = os.fsdecode(file)
         if "760" in filename:
-            h760_list.append(file)
+            h760_list.append(h_img)
             count = count + 1
         elif "770" in filename:
-            h770_list.append(file)
+            h770_list.append(h_img)
             count = count + 1
-        else: 
-            h780_list.append(file)
+        elif "780" in filename: 
+            h780_list.append(h_img)
             count = count + 1
+        else:
+            print("File did not match the specified format, input the nm in the name and then run again." + filename)
 
     h760 = sum(np.array(h760_list))
     h770 = sum(np.array(h770_list))
     h780 = sum(np.array(h780_list))
 
+    return h760, h770, h780
+
+    # print(len(h760_list))
+    # print(len(h770_list))
+    # print(len(h780_list))
+
 
 file_handling()
 
+# def main():
+#     h760 = 0
+#     h770 = 0
+#     h780 - 0
+    
+#     h760, h770, h780 = file_handling()
 
-
+#     histogram_processing(h760, h770, h780)
 
 
 
